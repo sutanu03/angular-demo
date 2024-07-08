3831ff4fb34090aa8ec12c4075334c0ed585ef7e
Sutanu Ghosal <147373998+sutanu03@users.noreply.github.com>
2024-07-08 09:16:25 +0530

    first angular project static

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+
+}
